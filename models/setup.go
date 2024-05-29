9cd1c7ca0aac97eb7305b591987d9853edd36d7f
Winar Joko Alexander <winaralex417@gmail.com>
2024-05-29 09:06:32 +0700

    add model config , history , status

--- models/setup.go
+++ models/setup.go
@@ -15,6 +15,47 @@ func ConnectDatabase() {
 
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&SensorData{})
-
+	db.AutoMigrate(&RelayStatus{})
+	db.AutoMigrate(&RelayConfig{})
+	db.AutoMigrate(&RelayHistory{})
 	DB = db
+
+	createRelayStatus()
+	createRelayConfig()
+}
+
+func createRelayStatus(){
+	relayStatus := RelayStatus{
+		Ph_up:   0,
+		Ph_down: 0,
+		Nut_A:   0,
+		Nut_B:   0,
+		Fan:     0,
+		Light:   0,
+		CreatedAt: time.Now(),
+	}
+
+	// Insert the new record into the database
+	result := db.Create(&relayStatus)
+	if result.Error != nil {
+		panic("failed to insert relay status record")
+	}
+}
+
+func createRelayConfig(){
+	relayConfig := RelayConfig{
+		Ph_up:   20,
+		Ph_down: 20,
+		Nut_A:   20,
+		Nut_B:   20,
+		Fan:     20,
+		Light:   20,
+		CreatedAt: time.Now(),
+	}
+
+	// Insert the new record into the database
+	result := db.Create(&relayConfig)
+	if result.Error != nil {
+		panic("failed to insert relay status record")
+	}
 }
