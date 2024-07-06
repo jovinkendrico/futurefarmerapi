08246ee4e56aff7ba91333905eb2d8411c93c6c4
jovinkendrico <jovinkendrico@gmail.com>
2024-07-06 17:23:32 +0700

    updat

--- controllers/datacontroller/datacontroller.go
+++ controllers/datacontroller/datacontroller.go
@@ -3,6 +3,7 @@ package datacontroller
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/jovinkendrico/futurefarmerapi/helper"
 	"github.com/jovinkendrico/futurefarmerapi/models"
@@ -36,7 +37,7 @@ func InsertData(w http.ResponseWriter, r *http.Request) {
 	temperatureStr := r.FormValue("temperature")
 	temperature, err := strconv.ParseFloat(temperatureStr, 64)
 	if err != nil {
-		http.Error(w, "Invalid temp value", http.StatusBadRequest)
+		http.Error(w, "Invalid temperature value", http.StatusBadRequest)
 		return
 	}
 
@@ -44,7 +45,7 @@ func InsertData(w http.ResponseWriter, r *http.Request) {
 	humidityStr := r.FormValue("humidity")
 	humidity, err := strconv.ParseFloat(humidityStr, 64)
 	if err != nil {
-		http.Error(w, "Invalid humid value", http.StatusBadRequest)
+		http.Error(w, "Invalid humidity value", http.StatusBadRequest)
 		return
 	}
 
@@ -69,51 +70,77 @@ func InsertData(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if ph < levelConfig.Ph_low {
-		relayStatus.Ph_up = 1
-		var relayHistory models.RelayHistory
-		relayHistory.Type = "PH UP"
-		relayHistory.Status = "on"
-		err := models.DB.Create(&relayHistory).Error
-		if err != nil {
-			panic("failed to insert relay history record")
+	now := time.Now()
+	checkInterval := -3 * time.Hour
+
+	var phUpHistory models.RelayHistory
+	result = models.DB.Where("type = ?", "PH UP").Order("created_at DESC").First(&phUpHistory)
+
+	if result.Error != nil || phUpHistory.CreatedAt.Before(now.Add(checkInterval)) {
+		if ph < levelConfig.Ph_low && relayStatus.Is_manual_1 == 0 {
+			relayStatus.Ph_up = 1
+			var relayHistory models.RelayHistory
+			relayHistory.Type = "PH UP"
+			relayHistory.Status = "ON"
+			err := models.DB.Create(&relayHistory).Error
+			if err != nil {
+				panic("failed to insert relay history record")
+			}
 		}
 	}
 
-	if ph > levelConfig.Ph_high {
-		relayStatus.Ph_down = 1
-		relayHistory := models.RelayHistory{
-			Type:   "PH DOWN",
-			Status: "ON",
-		}
-		result := models.DB.Create(&relayHistory).Error
-		if result != nil {
-			panic("failed to insert relay history record")
+	var phDownHistory models.RelayHistory
+	result = models.DB.Where("type = ?", "PH DOWN").Order("created_at DESC").First(&phDownHistory)
+	if result.Error != nil || phDownHistory.CreatedAt.Before(now.Add(checkInterval)) {
+		if ph > levelConfig.Ph_high && relayStatus.Is_manual_2 == 0 {
+			relayStatus.Ph_down = 1
+			relayHistory := models.RelayHistory{
+				Type:   "PH DOWN",
+				Status: "ON",
+			}
+			result := models.DB.Create(&relayHistory).Error
+			if result != nil {
+				panic("failed to insert relay history record")
+			}
 		}
 	}
+
 	if tds < levelConfig.Tds {
-		relayStatus.Nut_a = 1
-		relayStatus.Nut_b = 1
-		relayHistory := models.RelayHistory{
-			Type:   "NUTRISI A",
-			Status: "ON",
-		}
-		result := models.DB.Create(&relayHistory).Error
-		if result != nil {
-			panic("failed to insert relay history record")
-		}
-		relayHistory_2 := models.RelayHistory{
-			Type:   "NUTRISI B",
-			Status: "ON",
-		}
-		result_2 := models.DB.Create(&relayHistory_2).Error
-		if result_2 != nil {
-			panic("failed to insert relay history record")
+		var nutAHistory models.RelayHistory
+		result = models.DB.Where("type = ?", "NUTRISI A").Order("created_at DESC").First(&nutAHistory)
+		if result.Error != nil || nutAHistory.CreatedAt.Before(now.Add(checkInterval)) {
+			if relayStatus.Is_manual_3 == 0 {
+				relayStatus.Nut_a = 1
+
+				relayHistory := models.RelayHistory{
+					Type:   "NUTRISI A",
+					Status: "ON",
+				}
+				result := models.DB.Create(&relayHistory).Error
+				if result != nil {
+					panic("failed to insert relay history record")
+				}
+			}
 		}
 
+		var nutBHistory models.RelayHistory
+		result = models.DB.Where("type = ?", "NUTRISI B").Order("created_at DESC").First(&nutBHistory)
+		if result.Error != nil || nutBHistory.CreatedAt.Before(now.Add(checkInterval)) {
+			if relayStatus.Is_manual_4 == 0 {
+				relayStatus.Nut_b = 1
+				relayHistory_2 := models.RelayHistory{
+					Type:   "NUTRISI B",
+					Status: "ON",
+				}
+				result_2 := models.DB.Create(&relayHistory_2).Error
+				if result_2 != nil {
+					panic("failed to insert relay history record")
+				}
+			}
+		}
 	}
 
-	if temperature < levelConfig.Temperature_low || humidity < levelConfig.Humidity {
+	if (temperature > levelConfig.Temperature_high || humidity > levelConfig.Humidity) && relayStatus.Is_manual_5 == 0 {
 		relayStatus.Fan = 1
 		relayHistory := models.RelayHistory{
 			Type:   "FAN",
@@ -125,17 +152,17 @@ func InsertData(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if temperature > levelConfig.Temperature_high {
-		relayStatus.Light = 1
-		relayHistory := models.RelayHistory{
-			Type:   "LIGHT",
-			Status: "ON",
-		}
-		result := models.DB.Create(&relayHistory).Error
-		if result != nil {
-			panic("failed to insert relay history record")
-		}
-	}
+	// if temperature > levelConfig.Temperature_high  && relayStatus.Is_manual_6 == 0  {
+	// 	relayStatus.Light = 1
+	// 	relayHistory := models.RelayHistory{
+	// 		Type:   "LIGHT",
+	// 		Status: "ON",
+	// 	}
+	// 	result := models.DB.Create(&relayHistory).Error
+	// 	if result != nil {
+	// 		panic("failed to insert relay history record")
+	// 	}
+	// }
 
 	if saveResult := models.DB.Save(&relayStatus); saveResult.Error != nil {
 		http.Error(w, saveResult.Error.Error(), http.StatusInternalServerError)
@@ -150,41 +177,4 @@ func InsertData(w http.ResponseWriter, r *http.Request) {
 	// Respond with the inserted data
 	helper.ResponseJSON(w, http.StatusOK, data)
 
-}
-func GetRelayHistory(w http.ResponseWriter, r *http.Request) {
-	// Define pagination parameters
-	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
-	pageSize, _ := strconv.Atoi(r.URL.Query().Get("page_size"))
-	if page < 1 {
-		page = 1
-	}
-	if pageSize < 1 {
-		pageSize = 20
-	}
-	offset := (page - 1) * pageSize
-
-	// Retrieve relay history records with pagination and sorting by id
-	var totalRecordCount int64
-	var relayHistory []models.RelayHistory
-	models.DB.Model(&models.RelayHistory{}).Count(&totalRecordCount)
-	result := models.DB.Order("id desc").Limit(pageSize).Offset(offset).Find(&relayHistory)
-	if result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-	// Prepare the response data
-	responseData := struct {
-		PageNumber       int64                 `json:"page_number"`
-		PageSize         int64                 `json:"page_size"`
-		TotalRecordCount int64                 `json:"total_record_count"`
-		Records          []models.RelayHistory `json:"records"`
-	}{
-		PageNumber:       int64(page),
-		PageSize:         int64(pageSize),
-		TotalRecordCount: totalRecordCount,
-		Records:          relayHistory,
-	}
-
-	// Respond with the relay history data
-	helper.ResponseJSON(w, http.StatusOK, responseData)
 }
